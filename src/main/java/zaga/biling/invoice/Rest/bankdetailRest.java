7be143d2afd8615196511b8603999134cc5b4cc4
Anushiya <anushiyadhamu@gmail.com>
2023-07-05 14:31:38 +0530

    develop branch pr

--- src/main/java/zaga/biling/invoice/Rest/bankdetailRest.java
+++ src/main/java/zaga/biling/invoice/Rest/bankdetailRest.java
@@ -33,7 +33,7 @@ public class bankdetailRest {
     public Response createBankDetails(BankDetail bDetail) {
         try {
             BankDetail bDetailNew = bService.addBankDetail(bDetail);
-            System.out.println("BankDetail: ");
+            System.out.println("BankDetail 1: ");
             return Response.status(Response.Status.OK).entity(bDetailNew).build();
         } catch (Exception e) {
             {
